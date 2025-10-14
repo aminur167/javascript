fb96eadb8529e23e106c67bac08862de8903fad8
aminur167 <aminurhstu23@gmail.com>
2025-10-14 10:46:34 +0600

    string

---
+++ parseFloat_parseInt.js
@@ -0,0 +1,34 @@
+var num1="10";
+var num2=50;
+console.log(num1+num2);
+
+//string k number a convert korar niyom....
+num1=parseFloat(num1);
+console.log(num1+num2);
+
+//soto kore korte caile...
+var sum=num1+num2;
+console.log(sum);
+
+var num1="10.12345";
+var num2=50;
+console.log(num1+num2);
+
+//string k number a convert korar niyom....
+num1=parseFloat(num1);
+console.log(num1+num2);
+
+num1=parseInt(num1);
+console.log(num1+num2);
+
+
+//number to string convert 
+var num3=20;
+var num4=50;
+var sum=num3+num4;
+console.log(sum);
+
+num3=""+num3;
+num4=""+num4;
+var sum=num3+num4;
+console.log(sum);
