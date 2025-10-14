979b1fbc60399b6776a49ddea172d147dfb68a62
aminur167 <aminurhstu23@gmail.com>
2025-10-14 17:09:57 +0600

    operators

---
+++ operators.js
@@ -0,0 +1,17 @@
+var a=12;
+var b=3;
+
+var sum=a+b;
+console.log(sum);
+
+var sub=a-b;
+console.log(sub);
+
+var multi=a*b;
+console.log(multi);
+
+var div=a/b;
+console.log(div);
+
+var mod=a%b;
+console.log(mod);
