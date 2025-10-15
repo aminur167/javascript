b2ba74d2be6bf9a50ec2c7db9cde9188dbe1fde6
aminur167 <aminurhstu23@gmail.com>
2025-10-15 09:30:24 +0600

    ath.h

---
+++ ceil_floor.js
@@ -0,0 +1,17 @@
+var a=2.234;
+var result=Math.round(a);
+console.log(result);
+
+var result=Math.ceil(a);
+console.log(result);
+
+var result=Math.floor(a);
+console.log(result);
+
+var result=Math.random(a);
+console.log(result);
+
+//negative k positive kora
+var num=-20;
+var result=Math.abs(num);
+console.log(result);
