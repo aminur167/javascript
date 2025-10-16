91214cf7bd740f63862592a39eed047875db5267
aminur167 <aminurhstu23@gmail.com>
2025-10-16 17:25:45 +0600

    date

---
+++ DateObjects.js
@@ -0,0 +1,10 @@
+var myDate=new Date()
+
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getMilliseconds());
