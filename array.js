bd77ee8be26537b2029cfab611cf12e09b4a7501
aminur167 <aminurhstu23@gmail.com>
2025-10-17 12:59:10 +0600

    array

--- array.js
+++ array.js
@@ -1,2 +1,20 @@
 var fdName=["Amin","Momin","Hanif"];
 console.log(fdName[2]);
+
+//list a add kore.push diye just e add kora jai.
+fdName.push("Hamja");
+console.log(fdName);
+
+//list theke bad deuya. pop diye just last er element delete kora jai.
+fdName.pop();
+console.log(fdName);
+
+//suru theke bad dite shift use kora hoi.
+fdName.shift();
+console.log(fdName);
+
+//suru te add korte unshift use kora hoi.
+fdName.unshift("Queen");
+console.log(fdName);
+
+
---
+++ array_slice.js
@@ -0,0 +1,5 @@
+var fdName=["A","B","C","D","E"];
+
+//splice use kora isse moto delete add/kora jai;
+fdName.splice(1,2,"Z");
+console.log(fdName);
