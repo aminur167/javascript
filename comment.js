bb7c3135a0777b55ec46f4ea1c466961863f82c3
aminur167 <aminurhstu23@gmail.com>
2025-10-20 20:27:07 +0600

    comment

---
+++ comment.js
@@ -0,0 +1,9 @@
+// This is a single-line comment
+let x = 10; // This is also a single-line comment at the end of a line
+
+
+/* 
+  This is a multi-line comment.
+  You can write multiple lines here.
+*/
+let y = 20;
